e9d2ecbcdd4e5a71d57e22d1cb74190c27334033
Latios <kirby.elettro@gmail.com>
2025-01-12 19:29:42 +0100

    Volumes

---
+++ SeeSharp/Shading/Volumes/Volume.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SeeSharp.Shading.Volumes;
+
+public class HomogeneousVolume {
+    public string Name { get; init; }
+
+    public required float Sigma_a;
+    public required float Sigma_s;
+    public float Sigma_t => Sigma_a + Sigma_s;
+    public float Albedo => Sigma_s / Sigma_t;
+    //public float Mfp => 1.0f / Sigma_t;
+
+    public float Transmittance(Vector3 x, Vector3 y) {
+        return MathF.Exp(Sigma_t * (x - y).Length());
+    }
+
+
+
+}
